0ae2af7c73ac42a3ae81ef48904fb34f20f128b6
Brian Luebbert-Hill <BLuebbertHill@gmail.com>
2017-11-04 17:21:32 -0400

    added stuff that doesn't do much

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-function
+function go() {
+
+}
+
+function getTweets(topic) {
+    var tweets = [];
+    let url = "https://api.twitter.com/1.1/search/tweets.json?q=" + topic + "&result_type=popular&count=100"
+    
+
+    return tweets;
+}
+
